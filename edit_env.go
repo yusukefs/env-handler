031bfc77dd5d3ccb41e0c8a5006f58568b6d1566
Yusuke Sakai <my.gosh.love.ya@gmail.com>
2018-07-10 21:57:06 +0900

    Fix convert

--- edit_env.go
+++ edit_env.go
@@ -4,19 +4,23 @@ import (
   "strings"
 )
 
-func ConvertSingleLineTosample(line string) string {
-  line_split := strings.SplitN(line, "=", 2)
-  if len(line_split) == 1 {
+func ConvertSingleLineToSample(line string) string {
+  if string(([]rune(line)[0])) == "#" {
     return line
   } else {
-    return line_split[0] + "="
+    line_split := strings.SplitN(line, "=", 2)
+    if len(line_split) == 1 {
+      return line
+    } else {
+      return line_split[0] + "="
+    }
   }
 }
 
 func BuildValueRemovedLines(original_lines []string) []string{
   lines := make([]string, 0)
   for _, line := range original_lines {
-    lines = append(lines, ConvertSingleLineTosample(line))
+    lines = append(lines, ConvertSingleLineToSample(line))
   }
 
   return lines
--- edit_env_test.go
+++ edit_env_test.go
@@ -6,10 +6,18 @@ import (
 
 func TestGetKeyFromSingleLine(t *testing.T) {
   input := `TEST_1='test===tesrefe'`
-  actual := ConvertSingleLineTosample(input)
+  actual := ConvertSingleLineToSample(input)
   expected := `TEST_1=`
 
   if actual != expected {
     t.Errorf("Failed on removing value from single line. actual: %v, expected: %v", actual, expected)
+  }
+
+  input = `#TEST_1='test===tesrefe'`
+  actual = ConvertSingleLineToSample(input)
+  expected = `#TEST_1='test===tesrefe'`
+
+  if actual != expected {
+    t.Errorf("Failed on not removing when line starts from #")
   }
 }
