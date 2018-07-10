319f3e25559c7dac48e15d8a6a40b704dcb6557e
Yusuke Sakai <my.gosh.love.ya@gmail.com>
2018-07-10 20:28:15 +0900

    Fix test

--- edit_env_test.go
+++ edit_env_test.go
@@ -6,8 +6,8 @@ import (
 
 func TestGetKeyFromSingleLine(t *testing.T) {
   input := `TEST_1='test===tesrefe'`
-  actual := GetKeyFromSingleLine(input)
-  expected := `TEST_1`
+  actual := ConvertSingleLineTosample(input)
+  expected := `TEST_1=`
 
   if actual != expected {
     t.Errorf("Failed on removing value from single line. actual: %v, expected: %v", actual, expected)
